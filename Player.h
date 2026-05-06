a6fa4bda693307889ce5ee7daad5aaaa6844f89d
Simone <simoneiorio68@gmail.com>
2026-05-06 18:33:02 +0200

    Rimosse alcune funzioni che ritornavano valori al main

--- Player.h
+++ Player.h
@@ -11,11 +11,7 @@
 class Player : public Entity {
 protected:
     int life;               //numero di vite
-    bool isBombActive;      // Mi dice se c'è una bomba a terra
-    int bombX;              // Coordinata X della bomba
-    int bombY;              // Coordinata Y della bomba
 
-    int bombTimer;          // Cronometro della bomba
 public:
 
     Player(int X, int Y, char S, int LIFE);          //costruttore
@@ -29,27 +25,13 @@ public:
 
     void reset_position(); // Riporta il giocatore al punto di spawn
 
-    void bomb_placement();   //piazzamento bombe
-
     void collect_item();    //funzione per collezionare oggetti
 
     void move(int input, Livello* currentLevel );
 
+
+
     void check_teleport(Livello* currentLevel); //Controlla se attivare la funzione teletrasporto
-
-    bool getIsBombActive();
-
-    int getBombX();
-
-    int getBombY();
-
-    int getBombTimer();    // Per leggere a che punto è il timer
-
-    void tickBomb();       // Per far scorrere il tempo
-
-    void resetBomb();      // Per far sparire la bomba dopo l'esplosione
-
-
 
     //Aggiunto Blinking
     //Successivamente utile per anche l'immortalità
